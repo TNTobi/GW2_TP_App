41b09c9112be4f3a708c1ec85d87201932949623
Tobias N. Hilger <tobias.hilger1@gmail.com>
2015-12-19 13:43:06 +0100

    Inital Commit

---
+++ GW2_TP_App/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GW2_TP_App.MvcApplication" Language="C#" %>
---
+++ GW2_TP_App/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GW2_TP_App.Startup))]
+namespace GW2_TP_App
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
